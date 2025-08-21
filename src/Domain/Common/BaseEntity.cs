aacd559ba0bbb3da248c5d9d040226bc176c9f5c
arman <armanbarooni@gmail.com>
2025-08-21 18:08:57 +0330

    basic configs

---
+++ src/Domain/Common/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public int? MahakId { get; set; }
+        public long? MahakClientId { get; set; }
+        public long RowVersion { get; set; }
+        public bool Deleted { get; set; } = false;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public int CreatedBy { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public int? UpdatedBy { get; set; }
+    }
+}
---
+++ src/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineShop.Domain.Common;
+//using OnlineShop.Domain.Entities; // موجودیت‌هات
+
+namespace OnlineShop.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // مثال DbSet
+        //public DbSet<Product> Products { get; set; }
+        //public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                if (typeof(BaseEntity).IsAssignableFrom(entityType.ClrType))
+                {
+  
+                    modelBuilder.Entity(entityType.ClrType)
+                        .Property<long>(nameof(BaseEntity.RowVersion))
+                        .IsConcurrencyToken();
+
+                }
+            }
+        }
+
+    }
+}
